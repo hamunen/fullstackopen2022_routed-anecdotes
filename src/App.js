789f5ddbad60227f99d0ebadcc71d5ecadcfd1dd
joooda <henri.hamunen@gmail.com>
2022-09-05 21:46:17 +0300

    ex 7.6 - form reset button

--- src/App.js
+++ src/App.js
@@ -67,9 +67,10 @@ const Notification = ({notification}) => {
   
 
 const CreateNew = (props) => {
-  const content = useField('text')
-  const author = useField('text')
-  const info = useField('text')
+  // https://flaviocopes.com/how-to-rename-object-destructuring/
+  const { reset: resetContent, ...content} = useField('text')
+  const { reset: resetAuthor, ...author} = useField('text')
+  const { reset: resetInfo, ...info} = useField('text')
 
   const navigate = useNavigate()
 
@@ -82,6 +83,12 @@ const CreateNew = (props) => {
       votes: 0
     })
     navigate('/')
+  }
+
+  const handleReset = () => {
+    resetContent()
+    resetAuthor()
+    resetInfo()
   }
 
   return (
@@ -100,7 +107,8 @@ const CreateNew = (props) => {
           url for more info
           <input name='info' {...info} />
         </div>
-        <button>create</button>
+        <button type='submit'>create</button>
+        <button type='reset' onClick={handleReset}>reset</button>
       </form>
     </div>
   )
